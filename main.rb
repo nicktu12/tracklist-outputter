6a19c4342711c8d229eef39f6ab990a475d8a13c
Nick Teets <nickteets@gmail.com>
2024-12-13 10:24:44 -0700

    adds more animation background

--- main.rb
+++ main.rb
@@ -9,6 +9,7 @@ Curses.init_pair(2, Curses::COLOR_WHITE, Curses::COLOR_BLACK) # White text on bl
 Curses.init_pair(3, Curses::COLOR_BLUE, Curses::COLOR_BLACK) # Blue stars
 Curses.init_pair(4, Curses::COLOR_RED, Curses::COLOR_BLACK) # Red stars
 Curses.init_pair(5, Curses::COLOR_YELLOW, Curses::COLOR_BLACK) # Yellow stars
+Curses.init_pair(6, Curses::COLOR_MAGENTA, Curses::COLOR_BLACK) # Magenta stars for pulsing
 Curses.noecho
 Curses.curs_set(0)
 Curses.timeout = 0 # Non-blocking input
@@ -39,25 +40,45 @@ begin
   screen.keypad(true)
 
   # Shooting stars state
-  stars = Array.new(20) do
+  stars = Array.new(10) do
     {
       position: [rand(screen.maxy), rand(screen.maxx)],
-      size: rand(3..5),
+      size: rand(5..8),
       color: rand(1..5),
+      pulse: rand(0..1),
     }
   end
+
+  # Background grid effect
+  grid = Array.new(screen.maxy) { Array.new(screen.maxx) { rand(10) == 0 ? '.' : ' ' } }
 
   # Animation loop
   loop do
     screen.clear
+
+    # Draw background grid
+    screen.attron(Curses.color_pair(2)) do
+      grid.each_with_index do |row, y|
+        row.each_with_index do |cell, x|
+          screen.setpos(y, x)
+          screen.addstr(cell)
+        end
+      end
+    end
+
+    # Update grid for subtle animation
+    grid.each_with_index do |row, y|
+      row.map! { |cell| rand(10) == 0 ? '.' : ' ' }
+    end
 
     # Draw shooting stars
     stars.each do |star|
       y, x = star[:position]
       size = star[:size]
       color = star[:color]
+      pulse = star[:pulse]
 
-      screen.attron(Curses.color_pair(color)) do
+      screen.attron(Curses.color_pair(pulse.zero? ? color : 6)) do
         size.times do |i|
           break if y + i >= screen.maxy || x + i >= screen.maxx
           screen.setpos(y + i, x + i)
@@ -69,10 +90,13 @@ begin
       star[:position][0] += 1
       star[:position][1] += 1
 
+      # Toggle pulse state
+      star[:pulse] = 1 - star[:pulse]
+
       # Reset star position if it goes off screen
       if star[:position][0] >= screen.maxy || star[:position][1] >= screen.maxx
         star[:position] = [rand(screen.maxy / 4), rand(screen.maxx)]
-        star[:size] = rand(3..5)
+        star[:size] = rand(5..8)
         star[:color] = rand(1..5)
       end
     end
